9fdc9359bfd8067658f79ed0e148cb9dcaaae09e
Tiago <tiago.araujo.neo@gmail.com>
2017-11-16 20:56:43 -0200

    First commit

---
+++ app/src/main/java/com/example/tiago/helloworld/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.tiago.helloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    public ImageView imagem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imagem = (ImageView) findViewById(R.id.imgIcon);
+
+
+        imagem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IconActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public MainActivity() {
+        super();
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "You started the app", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "You restarted the app", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "You resumed the app", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "You paused the app", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "You stopped the app", Toast.LENGTH_LONG).show();
+    }
+}
